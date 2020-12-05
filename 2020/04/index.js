a270d12193000657251d1751af1031a84ef4b751
SeanM96 <sean-9708@hotmail.co.uk>
2020-12-05 21:06:39 +0000

    day 4 part 2 doesnt work

--- 2020/04/index.js
+++ 2020/04/index.js
@@ -1,27 +1,77 @@
 const fs = require('fs')
-const input = fs.readFileSync('./input2.txt').toString();
+const input = fs.readFileSync('./input.txt').toString();
+
 const requiredFields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
 
+const formattedInput = input.split('\n\n').map(e => (e.split('\n'))).map(nested => nested.join());
+const inputObjects = formattedInput.map((string) => {  // for each string in array
+    const pairs = string.split(/[\ ,]/);  // split by space or comma
+    const object = {};                    // create an object
+    for (pair of pairs) {                 // for each pair in string
+        const parts = pair.split(":");      // split by colon
+        if (parts.length == 2) {            // if you get 2 parts after splitting
+            object[parts[0]] = parts[1];      // use the first part as a key and the second as a value
+        }
+    }
+    return object;
+});
 
-const formattedInput = input.split(/\n\n+/)
-    .map(passport => passport.split(/[\n|\s]+/).flatMap(s => s.split(':')
-    ));
+const validEcl = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'];
+const pidRegex = "^\\d{9}$";
+const hexRegex = '(#[0-9a-f]{6})';
+const numRegex = '/\d+/g';
+const wordRegex = '/[a-z]+/g';
 
-function partOne() {
-    let includes = false;
-    return formattedInput.filter(el => {
-        includes = false;
-        requiredFields.every(field => {
-            if (el.includes(field)) {
-                includes = true;
-                return true;
-            } else {
-                includes = false;
-                return false;
-            }
-        });
-        return includes;
-    }).length;
+
+function passportFilter(passport) {
+    let valid = true;
+    const { byr, iyr, eyr, hgt, hcl, ecl, pid } = passport;
+    console.log(passport)
+    if (byr < 1920 || byr > 2002) {
+        console.log('INVALID BIRTH YEAR: ' + byr)
+        valid = false;
+    }
+    if (iyr < 2010 || iyr > 2020) {
+        console.log('INVALID ISSUE YEAR: ' + iyr)
+        valid = false;
+    }
+    if (eyr < 2020 || eyr > 2030) {
+        console.log('INVALID EXPIRATION YEAR: ' + eyr)
+        valid = false;
+    }
+    if (!validEcl.includes(ecl)) {
+        console.log('INVALID EYE COLOR: ' + ecl)
+        valid = false;
+    }
+    if (!new RegExp(pidRegex).test(pid)) {
+        console.log('INVALID PID: ' + pid)
+        valid = false;
+    }
+    if (!new RegExp(hexRegex).test(hcl)) {
+        console.log('INVALID HAIR COLOR: ' + hcl)
+        valid = false;
+    }
+    const height = hgt.match(numRegex) && hgt.match(numRegex)[0];
+    const measurement = hgt.match(wordRegex) && hgt.match(wordRegex)[0];
+    if (measurement === 'cm' && height < 150 || measurement === 'cm' && hgt > 193) {
+        valid = false;
+    }
+
+    if (measurement === 'in' && height < 59 || measurement === 'in' && hgt > 76) {
+        valid = false;
+    }
+    return valid;
 }
+function partOne() {
+    return inputObjects.filter(passport => {
+        return requiredFields.every(field => {
+            const containsField = Object.keys(passport).includes(field);
+            if (containsField) {
+                return true;
+            } else { return false };
+        });
+    });
+};
 
-console.log(partOne())
+const filtered = partOne().filter(passport => passportFilter(passport)).length;
+console.log(new RegExp(hexRegex).test('4a1444'));
