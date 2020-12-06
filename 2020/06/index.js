723ca0c6d0a98683a3be90e44e31916792ecf585
SeanM96 <sean-9708@hotmail.co.uk>
2020-12-06 15:14:14 +0000

    part 1

--- 2020/06/index.js
+++ 2020/06/index.js
@@ -1,15 +1,1 @@
-const fs = require('fs')
-const input = fs.readFileSync('./input.txt').toString();
-
-const formattedInput = input.split('\n\n').map(e => (e.split('\n'))).map(nested => nested.join());
-
-const removeDuplicateCharacters = string => (string
-    .split('')
-    .filter((element, index, arr) => (arr.indexOf(element) === index))
-    .join('').replace(',', ''));
-
-const partOne = formattedInput.
-    map(element => removeDuplicateCharacters(element).length).
-    reduce((a, b) => a + b);
-
-console.log(partOne)
+console.log(require('fs').readFileSync('./input.txt').toString().split('\n\n').map(e => (e.split('\n'))).map(nested => nested.join()).map(element => element.split('').filter((element, index, arr) => (arr.indexOf(element) === index)).join('').replace(',', '').length).reduce((a, b) => a + b));
