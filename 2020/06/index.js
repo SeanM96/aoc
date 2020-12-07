e0bb947d7679d22214d0760ecb1161ad9a387fc4
SeanM96 <sean-9708@hotmail.co.uk>
2020-12-07 17:59:48 +0000

    day 6 part 2!

--- 2020/06/index.js
+++ 2020/06/index.js
@@ -1,15 +1,26 @@
-const fs = require('fs')
+const fs = require('fs');
 const input = fs.readFileSync('./input.txt').toString();
+const formattedInput = input.split('\n\n').map(e => (e.split('\n')));
 
-const formattedInput = input.split('\n\n').map(e => (e.split('\n'))).map(nested => nested.join());
+const partOne = formattedInput.map(group => group.join('')).map(group => {
+    const set = new Set()
+    for (char in group) {
+        set.add(group[char])
+    }
+    return set.size
+}).reduce((a, b) => a + b);
 
-const removeDuplicateCharacters = string => (string
-    .split('')
-    .filter((element, index, arr) => (arr.indexOf(element) === index))
-    .join('').replace(',', ''));
+const partTwo = formattedInput.map( group => {
+    const set = new Set();
+    const sizeOfGroup = group.length;
+    const joined = group.join('');
+    for (char in joined) {
+        const numOfTimes = joined.split(joined[char]).length - 1;
+        if(numOfTimes === sizeOfGroup) {
+            set.add(joined[char])
+        }
+    }
+    return set.size;
+}).reduce((a, b) => a + b);;
 
-const partOne = formattedInput.
-    map(element => removeDuplicateCharacters(element).length).
-    reduce((a, b) => a + b);
-
-console.log(partOne)
+console.log(partTwo)
